3548718ae16e0a51f543f91ecc72a73dab17eb1a
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2024-12-28 12:34:48 +0000

    3.3.0

--- src/main/java/control/ImagePresenter.java
+++ src/main/java/control/ImagePresenter.java
@@ -8,13 +8,22 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ImagePresenter implements Presenter{
 
     private Image image;
-    private BufferedImage nextImage;
-    private BufferedImage prevImage;
+    /*A la hora de hacer el código, puedes cargar las imagenes previas y siguientes en onReleased, lo que optimizaría el onDragged,
+    pues ya tendrías cargada la imagen cuando arrastras, pero desfavorecería el onReleased, ya que tiene que cargar
+    la imagen previa y siguiente cada vez que sueltas. Por otro lado, podemos no hacer esto, de forma que el onReleased
+    estaría optimizado, ya que no debe hacer ninguna carga, pero el onDragged sería lento ya que debe cargar la próxima/anterior
+    imagen cada vez que arrastras. Me he decantado por favorecer el onReleased. Si quieres probar a favorecer el onDragged,
+    solo debes descomentar las líneas que hay comentadas en este archivo*/
+    //private BufferedImage nextImage;
+    //private BufferedImage prevImage;
     private final ImageDisplay imageDisplay;
+    private final Map<String, BufferedImage> imageCache;
 
 
 
@@ -22,23 +31,24 @@ public class ImagePresenter implements Presenter{
     public void show(Image image) {
         this.image = image;
         this.imageDisplay.paintImage(getBufferedImage(image.name()), 0);
+        /*
         this.prevImage = getBufferedImage(this.image.prev().name());
-        this.nextImage = getBufferedImage(this.image.next().name());
+        this.nextImage = getBufferedImage(this.image.next().name());*/
     }
 
     public ImagePresenter() {
         this.imageDisplay = new SwingImageDisplay();
         this.imageDisplay.onDragged(this::onDragged);
         this.imageDisplay.onReleased(this::onReleased);
-
+        this.imageCache = new HashMap<>();
     }
 
     private void onReleased(int offset){
         if (Math.abs(offset) > imageDisplay.width()/2) {
             this.image = offset < 0 ? image.next() : image.prev();
-            this.refresh();
+            this.refresh();/*
             this.prevImage = getBufferedImage(this.image.prev().name());
-            this.nextImage = getBufferedImage(this.image.next().name());
+            this.nextImage = getBufferedImage(this.image.next().name());*/
         }
         else{this.refresh();}
     }
@@ -48,10 +58,10 @@ public class ImagePresenter implements Presenter{
         this.imageDisplay.paintImage(getBufferedImage(image.name()), offset);
         if (offset == 0) return;
         if (offset > 0) {
-            this.imageDisplay.paintImage(prevImage, offset - this.imageDisplay.width());
+            this.imageDisplay.paintImage(getBufferedImage(image.prev().name()), offset - this.imageDisplay.width());
         }
         else{
-            this.imageDisplay.paintImage(nextImage, offset + this.imageDisplay.width());
+            this.imageDisplay.paintImage(getBufferedImage(image.next().name()), offset + this.imageDisplay.width());
         }
     }
 
@@ -64,8 +74,13 @@ public class ImagePresenter implements Presenter{
 
 
     private BufferedImage getBufferedImage(String name) {
+        if (imageCache.containsKey(name)) {
+            return imageCache.get(name);
+        }
         try {
-            return ImageIO.read(new File(name));
+            BufferedImage image = ImageIO.read(new File(name));
+            imageCache.put(name, image);
+            return image;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
